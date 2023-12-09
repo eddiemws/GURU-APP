157990e9ce2c96d44a90230eb2b5e97da8da5bee
eddy <106661971+eddiemws@users.noreply.github.com>
2023-12-09 23:43:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,2 @@
-import 'package:flutter/material.dart';
-import 'package:guruapp/app/screens/welcome/root.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-    @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Guru App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.red
-      ),
-      home: const WelcomePage(),
-    );
-  }
-}
 
 

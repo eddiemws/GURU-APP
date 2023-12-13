4067ac2b6f73e65614d81426d4995d1fb9ddb6e7
eddy <106661971+eddiemws@users.noreply.github.com>
2023-12-13 23:30:16 +0300

    Update root.dart

--- lib/app/screens/welcome/root.dart
+++ lib/app/screens/welcome/root.dart
@@ -1,8 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:guruapp/app/screens/root/root.dart';
-
-class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {

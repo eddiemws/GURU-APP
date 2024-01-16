5b646622b1635b4f1f5152e0dc7cf229db034127
eddy <106661971+eddiemws@users.noreply.github.com>
2024-01-16 22:33:34 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-    @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Guru App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.red
-      ),
-      home: const WelcomePage(),
-    );
-  }
-}
+
 
 
 

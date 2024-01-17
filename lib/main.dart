78160233b977555e0076125baf2d7dc20ebe7c89
eddy <106661971+eddiemws@users.noreply.github.com>
2024-01-17 23:58:50 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:guruapp/app/screens/welcome/root.dart';
 
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
